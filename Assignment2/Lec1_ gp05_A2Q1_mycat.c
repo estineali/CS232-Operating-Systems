d30f83a55fc18276c354ac7618252a90bc81bc60
Muhammad Shahrom Ali <estineali@gmail.com>
2019-10-15 01:45:07 +0500

    Initializing Comit

---
+++ Assignment2/Lec1_ gp05_A2Q1_mycat.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("This compiled script is called %s", argv[0]);
+	printf("Hello World\n");
+	return 0;
+}
