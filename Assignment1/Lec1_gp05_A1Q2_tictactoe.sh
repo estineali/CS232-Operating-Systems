3e2e70c3f8e4843e6720e38533ebe6e318a445ef
Muhammad Shahrom Ali <estineali@gmail.com>
2019-10-15 00:59:35 +0500

    Updating files

--- Assignment1/Lec1_gp05_A1Q2_tictactoe.sh
+++ Assignment1/Lec1_gp05_A1Q2_tictactoe.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-tttTable=("-" "-" "-" "-" "-" "-" "-" "-" "-")
+
+tttTable=(- - - - - - - - -)
 won=-1
+
+
+
+#checks to see if the computer of player has won
+whoWon(){
+  if [ ${tttTable[$1]} != "-" ] && [ ${tttTable[$1]} == ${tttTable[$2]} ] && [ ${tttTable[$2]} == ${tttTable[$3]} ]; then
+    won=1
+    if [ ${tttTable[$1]} == "0" ]; then echo -e "Player wins!"
+    else echo -e "\nComputer wins"
+    fi 
+    exit
+  fi
+}
+
+
+winsstates(){
+  whoWon 0 1 2
+  whoWon 0 4 8
+  whoWon 0 3 6
+  whoWon 1 4 7
+  whoWon 2 4 6
+  whoWon 2 5 8
+  whoWon 3 4 5
+  whoWon 6 7 8
+
+}
+
+
 
 
 tictactoe(){
@@ -9,8 +38,12 @@ tictactoe(){
 	while [ $won -eq -1 ]; do
 		#statements
 
+		echo "Enter row and column (x,y)"
+		read -r STR 
+		r=$(echo $STR | cut -f1 -d ',')
+		c=$(echo $STR | cut -f2 -d ',')
+
 		##Taking Input 
-		read -p "Enter row:" r
 		#Error Handling 
 		while [ $r -lt 0 ] || [ $r -gt 2 ] ; do
 			echo "Invalid Row Value. Row values are between 0 and 2"
@@ -18,7 +51,6 @@ tictactoe(){
 		done
 
 
-		read -p "Enter Column:" c
 		#Error Handling
 		while [ $c -lt 0 ] || [ $c -gt 2 ] ; do
 			echo "Invalid Column Value. Column values are between 0 and 2"
@@ -29,30 +61,21 @@ tictactoe(){
 		location=$(((r*3)+c))
 
 		#preventing overwriting of values
-		while [ $tttTable[$location] -eq 0 ] || [ $tttTable[$location] -eq 8 ]; do
-			echo "Box already selected."
-			read -p "Enter row:" r
-			
-			while [ $r -lt 0 ] || [ $r -gt 2 ] ; do
-				echo "Invalid Row Value. Row values are between 0 and 2"
-				read -p "Enter row:" r
-			done
-
-			read -p "Enter Column:" c
-			
-			while [ $c -lt 0 ] || [ $c -gt 2 ] ; do
-				echo "Invalid Column Value. Column values are between 0 and 2"
-				read -p "Enter Column:" c
-			done
-		
-			let location=$(((r*3)+c))
-
+		while [[ ${tttTable[$location]} != "-" ]]  ; do
+		    echo "You can't place there! Please try again:"
+		  		read -r STR
+				r=$(echo $STR | cut -f1 -d ',')
+				c=$(echo $STR | cut -f2 -d ',')
+				location=$(((r*3)+c))
+		    
 		done
+		let location=$(((r*3)+c))
+
 
 
 		##Setting the user's mark
 		let tttTable[$location]=0
-
+		winsstates
 
 		##Determining the computer's move
 		computerR=$(( ( RANDOM % 3 ) ))
@@ -62,7 +85,7 @@ tictactoe(){
 		computerlocation=$(((computerR*3)+computerC))	
 
 		#preventing overwriting of values
-		while [ $tttTable[$computerlocation] -eq 0 ] || [ $tttTable[$computerlocation] -eq 8 ]; do
+		while [ ${tttTable[$location]} != "-"]; do
 			let computerR=$(( ( RANDOM % 3 ) ))
 			let computerC=$(( ( RANDOM % 3 ) ))
 
@@ -72,7 +95,7 @@ tictactoe(){
 
 		##Setting the computer's mark
 		let tttTable[$computerlocation]=8
-
+		winsstates
 		##Displaying the grid
 		tput clear
 
--- Assignment1/Lec1_gp05_A1Q3_test_passwd.sh
+++ Assignment1/Lec1_gp05_A1Q3_test_passwd.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo Hello World
+paswd=$1
+
+if [[ ${#paswd} -lt 8 ]]; then
+	echo "Weak Password. Password is too short. if you have \$ in your password, write it as \\\$"
+elif [[ $paswd != *[123456789]* ]]; then
+	echo "Weak Password. Password must contain atleast 1 numeric character."
+elif [ $paswd != *[#\$\\%\&*+-=]* ]; then
+	echo "Weak Password. Password must contain atleast 1 special character."	
+else
+	echo "Good Password"
+fi
