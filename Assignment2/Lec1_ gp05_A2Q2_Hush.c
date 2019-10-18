b7e003001a1624e7f7a6b1b62f444cfc88d46e39
Muhammad Shahrom Ali <estineali@gmail.com>
2019-10-19 03:23:34 +0500

    Begun code

--- Assignment2/Lec1_ gp05_A2Q2_Hush.c
+++ Assignment2/Lec1_ gp05_A2Q2_Hush.c
@@ -1,9 +1,32 @@
+/** 
+Requirements: 
+	1. Cool prompt to prompt the user for input 
+	2. 
+
+**/
+
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
-	printf("This compiled script is called %s", argv[0]);
-	printf("Hello World\n");
+	printf("Hey! Welcome to HUSh.\n"); // Welcome command 
+
+	char inp_by_user[100]; //Contains user input 
+	const char prompt[] = "Your wish is my command: "; //The cool prompt it shows everytime  
+	
+
+	while (strncmp("exit", inp_by_user, 4) > 0)
+	{ // bug: if input is hello, program closes all the same 
+		
+		printf("%s", prompt);
+		scanf("%s", inp_by_user);
+		
+		//Whaetevr you intend to do with the input 
+		printf("\nYou said: %s \n\n", inp_by_user);
+	}
+
 	return 0;
 }
