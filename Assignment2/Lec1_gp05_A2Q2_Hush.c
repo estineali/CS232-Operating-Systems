80f353f84bc48a619b56f61a7f188948af2320e5
Muhammad Shahrom Ali <estineali@gmail.com>
2019-10-21 19:04:05 +0500

    Adding fork functionality

--- Assignment2/Lec1_gp05_A2Q2_Hush.c
+++ Assignment2/Lec1_gp05_A2Q2_Hush.c
@@ -1,47 +1,86 @@
-/** 
-Requirements: 
-	1. Cool prompt to prompt the user for input 
-	2. 
-
-**/
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int string_length(char* );
 
 int main(int argc, char const *argv[])
 {
+	int Exit = 0;
 
-	printf("\033c"); //Clear the terminal window
+	printf("\033cGreetings Master\n");
 
-	printf("Greetings Master\n");
-	// printf("Hey! Welcome to HUSh.\n"); // Welcome command 
-	char inps_by_user[100] = ""; //Contains user input 
-
-	const char prompt[] = "Master@HUSh: Your wish is my command$ "; //The cool prompt it shows everytime  
+	char* prompt = "Master@HUSh: Your wish is my command$ ";  
 	
-	//main Loop: run as long as not asked to exit 
+	int max_command_length = 255;
 
-	while (!(strncmp("exit", inps_by_user, 4) == 0))
+	while (!Exit)
 	{
 		printf("%s", prompt);
-		scanf("%s", inps_by_user);
 		
-		//Whaetevr you intend to do with the input
-		
+		char* command = malloc(max_command_length);
+		command = fgets(command, max_command_length, stdin);
 
-		//Exit Protocol 
-		if (strncmp("exit", inps_by_user, 4) == 0)
+		if (command == NULL)
 		{
-			printf("So be it, Master. I will wait for your return.\n");
-		} 
+			fprintf(stderr, "ERROR! With all due respect Master, what in the world are you on about?.\n");
+		}
+
 		else
 		{
-			printf("\nYou said: %s \n\n", inps_by_user);	
-		}
-		
-	}
-	return 0;
+			if (strncmp("exit", command, 4) == 0 && string_length(command) == 4)
+			{
+				//Kill Running processes
+				printf("So be it, Master. I will wait for your return.\n");
+				Exit = 1;
+			} 
+			else if (strncmp("clear", command, 5) == 0 && string_length(command) == 5)
+			{
+				printf("\033c");
+			} 
+			else 
+			{
+				printf("\nI dont understand your Wish, master  %s \n\n", command);	
+			}
+			// else 
+			// {
+			// 	int new_proc = fork();
 
+			// 	if (new_proc < 0) 
+			// 	{
+			// 		fprintf(stderr, "Invalid Fork master!\n");
+			// 		exit(1);
+			// 	} 
+
+			// 	else if (new_proc == 0) 
+			// 	{ // child (new process)
+			// 		printf("hello, I am child (pid:%d)\n", (int) getpid());
+			// 	} 
+			// 	else 
+			// 	{
+			// 		// parent goes down this path (main)
+			// 		int termed_proc = wait(NULL);
+			// 		printf("Terminated Process: %d\n", termed_proc);
+			// 		printf("hello, I am parent of %d (pid:%d)\n", new_proc, (int) getpid());
+			// 	}
+			// }
+
+		}
+		free(command);
+
+	}
+	return EXIT_SUCCESS;
+}
+
+
+int string_length(char* string)
+{
+	int i = 0;
+	while (string[i])
+	{
+		i++;
+	}
+	return i-1;
 }
