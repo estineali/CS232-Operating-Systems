d78f0046eb06e8b61265ea1b5dd838c8c558a8ac
Muhammad Shahrom Ali <estineali@gmail.com>
2019-10-21 14:16:52 +0500

    Make File and Hush

--- Assignment2/Lec1_gp05_A2Q2_Hush.c
+++ Assignment2/Lec1_gp05_A2Q2_Hush.c
@@ -12,23 +12,36 @@ Requirements:
 
 int main(int argc, char const *argv[])
 {
+
 	printf("\033c"); //Clear the terminal window
 
+	printf("Greetings Master\n");
 	// printf("Hey! Welcome to HUSh.\n"); // Welcome command 
-	char inp_by_user[100]; //Contains user input 
-	
-	const char prompt[] = "Master! Your wish is my command $ "; //The cool prompt it shows everytime  
-	
+	char inps_by_user[100] = ""; //Contains user input 
 
-	while (!(strncmp("exit", inp_by_user, 4) == 0))
-	{ // bug: if input is hello, program closes all the same 
-		
+	const char prompt[] = "Master@HUSh: Your wish is my command$ "; //The cool prompt it shows everytime  
+	
+	//main Loop: run as long as not asked to exit 
+
+	while (!(strncmp("exit", inps_by_user, 4) == 0))
+	{
 		printf("%s", prompt);
-		scanf("%s", inp_by_user);
+		scanf("%s", inps_by_user);
 		
-		//Whaetevr you intend to do with the input 
-		printf("\nYou said: %s \n\n", inp_by_user);
-	}
+		//Whaetevr you intend to do with the input
+		
 
+		//Exit Protocol 
+		if (strncmp("exit", inps_by_user, 4) == 0)
+		{
+			printf("So be it, Master. I will wait for your return.\n");
+		} 
+		else
+		{
+			printf("\nYou said: %s \n\n", inps_by_user);	
+		}
+		
+	}
 	return 0;
+
 }
