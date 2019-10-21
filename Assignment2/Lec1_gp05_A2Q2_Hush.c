8f9751420d12b1359fca621234e01d77b4f01dae
Muhammad Shahrom Ali <estineali@gmail.com>
2019-10-21 22:39:14 +0500

    AAAA CHECK IT OUT ITS BEAUTIFUL

--- Assignment2/Lec1_gp05_A2Q2_Hush.c
+++ Assignment2/Lec1_gp05_A2Q2_Hush.c
@@ -4,23 +4,39 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+//Prototypes
 int string_length(char* );
+void print_string_array(char* const array[]);
 
+
+//Main
 int main(int argc, char const *argv[])
 {
-	int Exit = 0;
-
 	printf("\033cGreetings Master\n");
-
-	char* prompt = "Master@HUSh: Your wish is my command$ ";  
 	
+
+	char *VAR[16];
+	int j = 0;
+	VAR[j] = strtok(getenv("PATH"), ":");
+	while (VAR[j] != NULL)
+	{	
+		j++;
+		VAR[j] = strtok(NULL, ":");
+	}
+	VAR[j] = NULL;
+
+	char* cwd = "./";
+	int Exit = 0;
+	char* prompt = "Master@HUSh: Your wish is my command$ ";  
 	int max_command_length = 255;
+
 
 	while (!Exit)
 	{
 		printf("%s", prompt);
-		
+
 		char* command = malloc(max_command_length);
+		
 		command = fgets(command, max_command_length, stdin);
 
 		if (command == NULL)
@@ -53,18 +69,57 @@ int main(int argc, char const *argv[])
 				if (new_proc < 0) 
 				{
 					fprintf(stderr, "Invalid Fork master!\n");
+					free(command);
 					exit(1);
 				} 
 
 				else if (new_proc == 0) 
-				{ // child (new process)
-					printf("hello, I am child (pid:%d)\n", (int) getpid());
+				{ 	
+
+					int no_of_arg = 100;
+					char* child_argv[no_of_arg];
+					int j = 0;
+
+					child_argv[j] = strtok(command, " ");
+
+					while (child_argv[j] != NULL)
+					{	
+						j++;
+						child_argv[j] = strtok(NULL, " ");
+					}
+					child_argv[j-1] = strtok(child_argv[j-1], "\n");
+					child_argv[j] = NULL; //END OF ARRAY 
+
+					char buffer[255];
+					int v = 0;
+					
+					while (execvp(buffer, child_argv) == -1)
+					{
+
+						strcpy(buffer, VAR[v]);
+						strcat(buffer, "/");
+						strcat(buffer, child_argv[0]);
+						// printf("%s\n", buffer);
+						v++;
+
+					}
+
+
+					// if (execvp(, child_argv) == -1 )
+					// {
+					// 	printf("execvp on %s failed\n", child_argv[0]);	
+					// }	
+
+					fprintf(stderr, "FAILED to load %s\n", child_argv[0]);
+
+					free(command);
+					exit(-1);
 				} 
 				else 
 				{
-					// parent goes down this path (main)
+					//PARENT
 					int termed_proc = wait(NULL);
-					printf("Terminated Process: %d\n", termed_proc);
+					// printf("Terminated Process: %d\n", termed_proc);
 					printf("hello, I am parent of %d (pid:%d)\n", new_proc, (int) getpid());
 				}
 			}
@@ -86,3 +141,15 @@ int string_length(char* string)
 	}
 	return i-1;
 }
+
+void print_string_array(char* const array[])
+{
+	int j = 0;
+	while (array[j] != NULL)
+	{
+		printf("%s\n", array[j]);
+		j++;
+	}
+}
+
+
