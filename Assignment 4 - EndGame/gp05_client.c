549fabbf6a394f12506ac6083f34028243c4014e
Fatima Nadeem <31242975+fat1nad@users.noreply.github.com>
2019-12-04 17:20:14 +0500

    Update gp05_client.c

--- Assignment 4 - EndGame/gp05_client.c
+++ Assignment 4 - EndGame/gp05_client.c
@@ -1,11 +1,100 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <pthread.h>
+// #include <sys/types.h> 
+// #include <sys/socket.h>
+
+// int main(int argc, char const *argv[])
+// {
+// 	printf("Hello Client.\n");
+// 	return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char const *argv[])
-{
-	printf("Hello Client.\n");
-	return 0;
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int main(int argc, char const* argv[]){
+
+  char HorIP[]= arg[1];  //the hostname or IP we are looking up
+  short port = arg[2];  //the port we are connecting on
+  char* clientname = arg[3];  //the client name
+
+  struct addrinfo *result;  //to store results
+  struct addrinfo hints;  //to indicate information we want
+
+  struct sockaddr_in *saddr_in;  //socket internet address
+
+  int s,n;  //for error checking
+
+  int sock;  //socket file descriptor
+
+  // //char request[]="GET /index.html\n\r"; //the GET request
+  // char request[]="GET /index.html\r\n\r\n"; //the GET request
+  // //char request[]= "GET / HTTP/1.0\r\n\r\n";
+
+  // char response[4096];           //read in 4096 byte chunks
+
+
+  //setup our hints
+  memset(&hints, 0, sizeof(struct addrinfo));  //zero out hints
+  hints.ai_family = AF_INET; //we only want IPv4 addresses
+
+  //Convert the hostname to an address
+  if((s = getaddrinfo(HorIP, NULL, &hints, &result)) != 0)
+  {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+    exit(1);
+  }
+
+  //convert generic socket address to internet socket address
+  saddr_in = (struct sockaddr_in*) result->ai_addr;
+  //set the port in network byte order
+  saddr_in->sin_port = htons(port);
+
+  //open a socket
+  if((sock = socket(AF_INET, SOCK_STREAM, 0))  < 0)
+  {
+    perror("socket");
+    exit(1);
+  }
+
+  //connect to the server
+  if(connect(sock, (struct sockaddr *)saddr_in, sizeof(*saddr_in)) < 0)
+  {
+    perror("connect");
+    exit(1);
+  }
+
+  //send the request
+  if(write(sock, request, strlen(request)) < 0)
+  {
+    perror("send");
+  }
+
+  //read the response until EOF
+  while( (n = read(sock, response, 4096)) > 0)
+  {
+    //write response to stdout
+    if(write(1, response, n) < 0)
+    {
+      perror("write");
+      exit(1);
+    }
+  }
+
+  if (n < 0)
+  {
+    perror("read");
+  }
+
+  //close the socket
+  close(sock);
+
+  return 0; //success
 }
