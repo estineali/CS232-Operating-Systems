76f486c9cabcd890d40932f527748d56eab7adca
Muhammad Shahrom Ali <ma03559@st.habib.edu.pk>
2019-12-04 13:43:51 +0500

    fixing structure

---
+++ Assignment 4 - EndGame/gp05_server.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello Server!\n");
+	return 0;
+}
