9b7ad04266cd9a49bea0d8ac0de7ece96ebdbd50
Muhammad Shahrom Ali <ma03559@st.habib.edu.pk>
2019-12-04 22:14:00 +0500

    Working to abstract workings for multithreading

--- Assignment 4 - EndGame/gp05_server.c
+++ Assignment 4 - EndGame/gp05_server.c
@@ -35,63 +35,54 @@ typedef struct server_class
 
 typedef struct client_class
 {
-
+	struct sockaddr_in client_saddr_in;
+	int client_sock;
 	
 } client_t;
 
 //Prototypes
 void set_up_server(server_t* s, short argv_port);
+void* connect_to_client(client_t* c, server_t* s);
+
 
 //Main
 int main(int argc, char* argv[])
 {
-
-	struct sockaddr_in client_saddr_in;  //socket internet address of client
-	int client_sock;
-
-	server_t main_server;
-	set_up_server(&main_server, atoi(argv[1]));
-	
-	printf("Listening On: %s:%d\n", 
-		inet_ntoa(main_server.saddr_in.sin_addr), 
-		ntohs(main_server.saddr_in.sin_port));
-	
-	char response[BUF_SIZE];  //what to send to the client
-	int n;  //length measure
-	
-	//accept incoming connections
-	if((client_sock = accept(main_server.server_sock, (struct sockaddr *) &client_saddr_in, &main_server.saddr_len)) < 0)
+	if (argv[1] == NULL)
 	{
-		perror("accept");
+		printf("No port mentioned. Exiting now...\n");
 		exit(1);
 	}
-	
-	printf("Connection From: %s:%d (%d)\n",
-		inet_ntoa(client_saddr_in.sin_addr), //address as dotted quad
-		ntohs(client_saddr_in.sin_port), //the port in host order
-		client_sock); //the file descriptor number
+
+	server_t main_server;
+	client_t client;
+
+	set_up_server(&main_server, atoi(argv[1]));
+	connect_to_client(&client, &main_server);
 		
 	//read from client
-	if((n = read(client_sock,response, BUF_SIZE-1)) < 0)
+	char response[BUF_SIZE];
+	int n = read(client.client_sock, response, BUF_SIZE-1);  //bytes read from client 
+	if(n < 0)
 	{
 		perror("read");
 		exit(1);
 	}
-	
 	response[n] = '\0'; //NULL terminate string
+	
 	printf("Read from client: %s\n", response);
 	
 	//construct response
 	snprintf(response, BUF_SIZE, "Hello %s:%d \nGo Navy! Beat Army\n",
-		inet_ntoa(client_saddr_in.sin_addr), //address as dotted quad
-		ntohs(client_saddr_in.sin_port)); //the point main(int argc, char* argv[])
+		inet_ntoa(client.client_saddr_in.sin_addr), //address as dotted quad
+		ntohs(client.client_saddr_in.sin_port)); //the point main(int argc, char* argv[])
 	// int serv_socket = socket(AF_INET, SOCK_STREAM, 0);
 	// rt in host order
 	
 	printf("Sending: %s",response);
 	
 	//send response
-	if(write(client_sock, response, strlen(response)) < 0)
+	if(write(client.client_sock, response, strlen(response)) < 0)
 	{
 		perror("write");
 		exit(1);
@@ -99,17 +90,22 @@ int main(int argc, char* argv[])
 	
 	printf("Closing socket\n\n");
 	
-	//close client_sock
-	close(client_sock);
-	
-	//close the socket
+	//close sockets
+	close(client.client_sock);
 	close(main_server.server_sock);
-	
 	return 0; //success
 }
 
 void set_up_server(server_t* s, short argv_port)
 {
+	/*
+		Function that sets up a server with all initial values. 
+		- intializing the socket
+		- binding the socket to the IP
+		- start listening 
+
+	*/
+
 	//Setting Values
 	s->server_IP = "127.0.0.1";
 	s->port = argv_port;
@@ -136,11 +132,36 @@ void set_up_server(server_t* s, short argv_port)
 		exit(1);
 	}
 
-	int queue_size = 5;
 	//ready to listen, queue up to queue_size pending connectinos
+	int queue_size = 5;
 	if(listen(s->server_sock, queue_size) < 0)
 	{
 		perror("listen");
 		exit(1);
 	}
+
+	printf("Listening On: %s:%d\n", 
+		inet_ntoa(s->saddr_in.sin_addr), 
+		ntohs(s->saddr_in.sin_port));
+}
+
+void* connect_to_client(client_t* c, server_t* s)
+{
+	c->client_sock = accept(s->server_sock, (struct sockaddr *) &c->client_saddr_in, &s->saddr_len);
+	
+	if(c->client_sock < 0)
+	{
+		perror("accept");
+		exit(1);
+	}
+	
+	printf("Connection From: %s:%d (%d)\n", 
+		inet_ntoa(c->client_saddr_in.sin_addr), 
+		ntohs(c->client_saddr_in.sin_port), 
+		c->client_sock); 
+		
+		//printing:
+			//address as dotted quad
+			//the port in host order
+			//the file descriptor number
 }
